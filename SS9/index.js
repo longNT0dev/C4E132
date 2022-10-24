a072a349901c44e425ba8cc119f244543f522850
longNT0dev <codeinlife10@gmail.com>
2022-10-24 15:07:54 +0700

    SS10

---
+++ SS9/index.js
@@ -0,0 +1,9 @@
+// 1. Khai báo 1 mảng gồm các số bất kỳ. 
+// Tính tích của các phần tử trong mảng đó.
+// Tìm số nhỏ nhất mà chia hết cho 2 trong mảng.
+// Tìm số lớn nhất mà chia hết cho 3 trong mảng.
+// Tính giá trị trung bình của mảng.
+// Lọc ra các số nguyên tố trong mảng.
+// Kiểm tra xem trong mảng có số nhỏ hơn 10 hay không.
+// Kiểm tra xem tất cả phần tử trong mảng có lớn hơn 20 không.
+// Nhập vào số n cho đến khi n là 1 số trong mảng s.
